7802194f4852c36d89c50d75ed17d170de0a2f85
Muhammet <muhammetsarican@gmail.com>
2023-09-17 22:42:02 +0300

    theme imported from semantic ui.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,119 @@
 import logo from './logo.svg';
 import './App.css';
 import { Link, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { InView } from 'react-intersection-observer'
+import "semantic-ui-css/semantic.min.css"
+
 import MoviePage from "./Components/Pages/MoviePage"
 
 import React, { Component } from 'react'
 
+import {
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
+
+import { menuStyle, fixedMenuStyle } from './Helpers/style.helper';
+
 export class App extends Component {
+  state = {
+    menuFixed: null,
+    overlayFixed: false,
+  }
   render() {
-    {console.log(this.props)}
+    { console.log(this.props) }
+    const { menuFixed} = this.state
+
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Link to="/movies">
-          Movies
-        </Link>
-      </header>
-      <Route path="/movies" component={MoviePage}></Route>
-    </div>
+        <InView onChange={this.toggleTopMenu}>
+          <Menu
+            borderless
+            fixed={menuFixed ? 'top' : null}
+            style={menuFixed ? fixedMenuStyle : menuStyle}
+          >
+
+            <Container text>
+              <Menu.Item>
+                <Image size='mini' src='https://react.semantic-ui.com/logo.png' />
+              </Menu.Item>
+              <Menu.Item header>Turkish Movies</Menu.Item>
+              <Menu.Item as={Link} to="/movies">
+                  Movies
+                </Menu.Item>
+              <Menu.Item as='a'>Add New</Menu.Item>
+            </Container>
+          </Menu>
+        </InView>
+        <Container style={{height:"100dvh"}}>
+          <Route path="/movies" component={MoviePage}></Route>
+        </Container>
+
+        <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
+          <Container textAlign='center'>
+            <Grid columns={4} divided stackable inverted>
+              <Grid.Row>
+                <Grid.Column>
+                  <Header inverted as='h4' content='Group 1' />
+                  <List link inverted>
+                    <List.Item as='a'>Link One</List.Item>
+                    <List.Item as='a'>Link Two</List.Item>
+                    <List.Item as='a'>Link Three</List.Item>
+                    <List.Item as='a'>Link Four</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column>
+                  <Header inverted as='h4' content='Group 2' />
+                  <List link inverted>
+                    <List.Item as='a'>Link One</List.Item>
+                    <List.Item as='a'>Link Two</List.Item>
+                    <List.Item as='a'>Link Three</List.Item>
+                    <List.Item as='a'>Link Four</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column>
+                  <Header inverted as='h4' content='Group 3' />
+                  <List link inverted>
+                    <List.Item as='a'>Link One</List.Item>
+                    <List.Item as='a'>Link Two</List.Item>
+                    <List.Item as='a'>Link Three</List.Item>
+                    <List.Item as='a'>Link Four</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column>
+                  <Header inverted as='h4' content='Footer Header' />
+                  <p>
+                    Extra space for a call to action inside the footer that could help re-engage
+                    users.
+                  </p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <Divider inverted section />
+            <Image src='https://react.semantic-ui.com/logo.png' centered size='mini' />
+            <List horizontal inverted divided link size='small'>
+              <List.Item as='a' href='#'>
+                Site Map
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Contact Us
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Terms and Conditions
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Privacy Policy
+              </List.Item>
+            </List>
+          </Container>
+        </Segment>
+      </div>
     )
   }
 }
